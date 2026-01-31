ffeefa54ae8a16a23a77de416cf4b3c39aca2f77
Subham Sahu <ssahu38170@gmail.com>
2026-01-31 11:50:26 +0530

    Setup Fastapi server

--- Backend/main.py
+++ Backend/main.py
@@ -1,1 +1,25 @@
-print("Hello")
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+
+
+@asynccontextmanager
+async def life_span(
+    app: FastAPI
+):
+    print(f'Server is starting...')
+
+    yield
+    print(f'Server has been stopped...')
+    
+
+
+
+app = FastAPI(
+    title='Rentalysis',
+    description='A REST API for a Rental Management System',
+    contact={
+        'email' : 'subham.sahu.cs@gmail.com'
+    },
+    lifespan=life_span
+)
